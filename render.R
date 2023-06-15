cf33cc1430865b515c2850c4c9e6dc330a91c6a5
florisvdh <floris.vanderhaeghe@inbo.be>
2023-06-15 14:55:46 +0200

    Fixes wrt relative paths to R figures

--- render.R
+++ render.R
@@ -5,6 +5,13 @@ dir.create("docs", showWarnings = FALSE)
 xfun::Rscript_call(
     rmarkdown::render,
     list(input = "qgisprocess_foss4g.Rmd",
-         output_file= file.path(gitroot,
-                                "docs/index"))
+         output_file= "index")
 )
+unlink("docs/index_files", recursive = TRUE)
+unlink("docs/libs/header-attrs", recursive = TRUE)
+unlink("docs/libs/remark-css", recursive = TRUE)
+file.rename("index.html", "docs/index.html")
+file.rename("index_files", "docs/index_files")
+file.rename("libs/header-attrs", "docs/libs/header-attrs")
+file.rename("libs/remark-css", "docs/libs/remark-css")
+unlink("libs", recursive = TRUE)
